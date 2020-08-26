75530df85be426c744fccf5455b81ddddfa6d2bf
yuchenliu <yuchenliu@synology.com>
2020-08-26 16:11:05 +0800

    Add Setup 10KB sparse file, with ext4 format and flex feature

---
+++ scripts/flexext4setup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Create a 10M sparse file, see: https://en.wikipedia.org/wiki/Sparse_file
+filename="../data/flex.img"
+dd of=$filename bs=10M seek=1 count=0
+
+# Associate loop devices with regular files or block devices
+# see: https://linux.die.net/man/8/losetup
+# alos see: https://gitmemory.com/issue/marcone/teslausb/306/539802870
+next_dev_loop=$(losetup -f | cut -f3)
+echo "next_dev_loop=$next_dev_loop"
+losetup $next_dev_loop "$filename"
+
+# Format it as ext4, with flex block group feature
+mkfs.ext4 -O flex_bg $next_dev_loop
+
+echo "To undo losetup:"
+echo "losetup -d $next_dev_loop"
