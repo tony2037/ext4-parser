7f5a84e75bd3c77cf5f134939fe4424d6b3286b5
yuchenliu <yuchenliu@synology.com>
2020-08-27 15:02:37 +0800

    Typo

--- scripts/metabg.half.sh
+++ scripts/metabg.half.sh
@@ -7,20 +7,22 @@ dd if=/dev/zero of=ext4_2T bs=4k count=1 seek=$((256*1024*1024*2))
 
 # associate with loop device
 echo "associate with loop device"
-losetup /dev/loop0 ext4_15T
-losetup /dev/loop1 ext4_2T
+next_dev_loop0=$(losetup -f | cut -f3)
+losetup $next_dev_loop0 ext4_15T
+next_dev_loop1=$(losetup -f | cut -f3)
+losetup $next_dev_loop1 ext4_2T
 
 # lvm 17T files
 echo "lvm"
 echo "vim /etc/lvm/lvm.conf, change filter to accept all device"
 sleep 2
 vim /etc/lvm/lvm.conf
-pvcreate /dev/loop0 /dev/loop1
-vgcreate vg_meta /dev/loop0 /dev/loop1
-lvcreate -l 100%FREE -n lv1 vg_meta
+pvcreate $next_dev_loop0 $next_dev_loop1
+vgcreate vg_meta $next_dev_loop0 $next_dev_loop1
+lvcreate -l 100%FREE -n lv1 vg_midMeta
 
 echo "mkfs.ext4"
-mkfs.ext4 -O 64bit /dev/vg_meta/lv1
+mkfs.ext4 -O 64bit /dev/vg_midMeta/lv1
 
 echo "Create lost+found"
 mount /dev/vg_midMeta/lv1 /mnt
@@ -29,11 +31,14 @@ mkdir lost+found
 umount /mnt
 
 echo "Add 2T into lvm"
-vgextend vg_midMeta /dev/loop1
-lvextend /dev/vg_midMeta/lv1 /dev/loop1
+vgextend vg_midMeta $next_dev_loop1
+lvextend /dev/vg_midMeta/lv1 $next_dev_loop1
 
 echo "resize to get meta_bg"
-mount /dev/vg_midMeta/lv1 mnt
+mount /dev/vg_midMeta/lv1 /mnt
 resize2fs -f /dev/vg_midMeta/lv1
 
 echo "Test data is ready!"
+echo "Remember.."
+echo "losetup -d $next_dev_loop0"
+echo "losetup -d $next_dev_loop1"
--- scripts/metabg.regular.sh
+++ scripts/metabg.regular.sh
@@ -7,18 +7,23 @@ dd if=/dev/zero of=ext4_2T bs=4k count=1 seek=$((256*1024*1024*2))
 
 # associate with loop device
 echo "associate with loop device"
-losetup /dev/loop0 ext4_15T
-losetup /dev/loop1 ext4_2T
+next_dev_loop0=$(losetup -f | cut -f3)
+losetup $next_dev_loop0 ext4_15T
+next_dev_loop1=$(losetup -f | cut -f3)
+losetup $next_dev_loop1 ext4_2T
 
 # lvm 17T files
 echo "lvm"
 echo "vim /etc/lvm/lvm.conf, change filter to accept all device"
 sleep 2
 vim /etc/lvm/lvm.conf
-pvcreate /dev/loop0 /dev/loop1
-vgcreate vg_meta /dev/loop0 /dev/loop1
+pvcreate $next_dev_loop0 $next_dev_loop1
+vgcreate vg_meta $next_dev_loop0 $next_dev_loop1
 lvcreate -l 100%FREE -n lv1 vg_meta
 echo "mkfs.ext4"
 mkfs.ext4 -O 64bit,meta_bg /dev/vg_meta/lv1
 
 echo "Test data is ready!"
+echo "Remember.."
+echo "losetup -d $next_dev_loop0"
+echo "losetup -d $next_dev_loop1"
